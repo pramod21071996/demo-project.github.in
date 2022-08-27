f44ef104d197088a4d31dfb567c02e331f3c2f76
pramod21071996 <pramodmore1996@gmail.com>
2022-08-27 17:59:34 +0530

    added styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MaterialModule } from 'src/material.module';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ResumeViewerComponent } from './resume-viewer/resume-viewer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -23,6 +24,7 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
+    NgbModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
